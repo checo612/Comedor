853496257e6acffa4b7dcb2d4419e712dfe30c1b
checo612 <40772974+checo612@users.noreply.github.com>
2018-08-09 17:29:01 -0500

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MisOrdenesComponent } from './components/mis-ordenes/mis-ordenes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'miOrden',
+  pathMatch: 'full',
+  component: MisOrdenesComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
