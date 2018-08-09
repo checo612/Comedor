303efc21945e4bf7dc4efd19dc0f1c14d535a297
alxdev91 <40773006+alxdev91@users.noreply.github.com>
2018-08-09 17:29:31 -0500

    antes de servicios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MenuComponent } from './components/pedidos/menu/menu.component';
+import { MisOrdenesComponent } from './components/mis-ordenes/mis-ordenes.component';
+import { ChefOrdenesComponent } from './components/chef-ordenes/chef-ordenes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'menu' , component: MenuComponent},
+  { path: 'mis_pedidos' , component: MisOrdenesComponent },
+  { path: 'listado_ordenes', component: ChefOrdenesComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
