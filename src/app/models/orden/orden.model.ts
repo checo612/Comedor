43a33b1b3f55cb718235c79958ecc1a5bc30fa1b
checo612 <40772974+checo612@users.noreply.github.com>
2018-08-10 14:26:29 -0500

    mejore el diseño

--- src/app/models/orden/orden.model.ts
+++ src/app/models/orden/orden.model.ts
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
 export interface Orden {
     usuario?: string;
     estatus: boolean;
     platillos: {
         platillo: string;
     };
-=======
-export class Orden {
->>>>>>> e5fad6b859a4e1f850fc7497af8b3f72efaac75d
 }
